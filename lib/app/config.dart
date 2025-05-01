56b4919609489eda2db1c5720c089cb5d4f7ef1d
babaissandiaye20 <babaissa,diaye31@gmail.com>
2025-05-01 20:21:33 +0000

    config

---
+++ lib/app/config.dart
@@ -0,0 +1,18 @@
+class Config {
+  // URL de l'API pour le backend NestJS
+  static const String baseUrl = "http://localhost:3000"; // Pour développement local
+
+  // Autres configurations potentielles
+  static const String apiVersion = "v1";
+  static const String authEndpoint = "/auth";
+  static const String uploadEndpoint = "/upload";
+  static const String appointmentEndpoint = "/appointments";
+
+  // Vous pouvez également configurer les environnements (par exemple, développement, production)
+  static String getApiUrl() {
+    return "$baseUrl/$apiVersion";
+  }
+
+  // Exemple de configuration pour des clés API ou autre si nécessaire
+  static const String apiKey = "votre_clé_api_ici";
+}
